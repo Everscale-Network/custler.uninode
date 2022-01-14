c75f4b2ad69416507e344b6d34f83dc33ceb6f00
Custler <svt@zmail.ru>
2022-01-14 22:15:48 +0300

    Fixed Get_Account_Info funcion due to tonos-cli output changed

--- scripts/env.sh
+++ scripts/env.sh
@@ -120,7 +120,7 @@ export C_ENGINE_ADDITIONAL_PARAMS=""
 # GIT addresses & commits
 export RUST_VERSION="1.57.0"
 export BOOST_VERSION="1.76.0"
-export MIN_TC_VERSION="0.22.12"
+export MIN_TC_VERSION="0.24.11"
 export MIN_RC_VERSION="0.1.255"
 
 export CNODE_GIT_REPO="https://github.com/Everscale-Network/Everscale-Node.git"
