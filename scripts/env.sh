a7e8e5b9b0b3ecd3da0e8a2e644f2483fdf826c2
Custler <svt@zmail.ru>
2022-01-29 17:19:10 +0300

    Updated Rust version to 1.58.0

--- scripts/env.sh
+++ scripts/env.sh
@@ -118,7 +118,7 @@ export C_ENGINE_ADDITIONAL_PARAMS=""
 
 #=====================================================
 # GIT addresses & commits
-export RUST_VERSION="1.57.0"
+export RUST_VERSION="1.58.0"
 export BOOST_VERSION="1.76.0"
 export MIN_TC_VERSION="0.24.11"
 export MIN_RC_VERSION="0.1.255"
